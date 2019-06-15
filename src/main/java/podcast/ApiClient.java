9bffeb98490c13b22e8f14c8cc260f6388d63cbd
Josh Long <josh@joshlong.com>
2019-06-14 22:54:59 -0700

    the end-to-end integration is starting to take shape..

--- src/main/java/podcast/ApiClient.java
+++ src/main/java/podcast/ApiClient.java
@@ -35,13 +35,16 @@ class ApiClient {
 		var body = new LinkedMultiValueMap<String, Object>();
 		body.add("file", resource);
 		var requestEntity = new HttpEntity<MultiValueMap<String, Object>>(body, headers);
-		var response = restTemplate.postForEntity(serverUrl, requestEntity, String.class);
+		var url = this.serverUrl + "?id=" + UUID.randomUUID().toString();
+		var response = restTemplate.postForEntity(url, requestEntity, String.class);
 		return response.getStatusCode().is2xxSuccessful();
 	}
 
 	public static void main(String[] args) {
 		var rt = new RestTemplateBuilder().build();
-		ApiClient apiClient = new ApiClient("http://localhost:8080/production?id=" + UUID.randomUUID().toString(), rt);
+		ApiClient apiClient = new ApiClient(
+			"http://localhost:8080/production?id=" + UUID.randomUUID().toString(),
+			rt);
 		var file = new File("/Users/joshlong/Desktop/sample-package.zip");
 		var sent = apiClient.publishPackage(file);
 		log.info("sent: " + sent);
